e771f39fc18ba6240a26d22df4a5e61931295b24
ethioboyjohnny <jonathanyohannes005@gmail.com>
2024-11-26 23:45:02 -0500

    Add files via upload

---
+++ WebDevelopmentLab03/infoj.py
@@ -0,0 +1,72 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpg"
+about_me = "I'm Jonathan Yohannes. I am a student at Georgia Tech, and I am a Biology student. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/spongebob-squarepants-44b857281/"
+my_github_url = "https://github.com/spongebob-squarepants"
+my_email_address = "spongebob@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Biology',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2027',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "BIOS 2300", "CHEM 1212", "BIOS 4540"], 
+    "names":["Intro to CS", "Ecology", "Chemistry 2", "Human Motor Control"], 
+    "semester_taken":["3rd", "3rd", "2nd", "3rd"],
+    "skills":["Python is not my favorite snake anymore", "Plants are really boring", "Sleep becomes an option", "Anatomy can get worse"],
+    }
+experience_data = {
+    "Tutor": (["- Tutored student as a highschool senior",
+                                                                          "- Increased confidence in students", "- Paid very little"],"Images/tutor.jpg"),
+
+}
+
+projects_data = {
+    "Fundraiser": "Started a fundraiser with friends for a downtown Atlanta hospital",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Read and Write",
+}
+leadership_data = {
+    "NHS president": (["- Started projects to help the community"],"Images/leader.jpeg"),
+
+}
+activity_data={
+    "Get Up and Go": ["- Force friends to come outside and have fun", 
+            "- Loves biking"]
+}
