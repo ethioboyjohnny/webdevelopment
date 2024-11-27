8cb265b9f74ebd074a262be455aa3c849bb8c875
ethioboyjohnny <jonathanyohannes005@gmail.com>
2024-11-26 23:49:00 -0500

    Update Jonathan's Portfolio.py

--- WebDevelopmentLab03/pages/Jonathan's Portfolio.py
+++ WebDevelopmentLab03/pages/Jonathan's Portfolio.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import info
+import infoj
 import pandas as pd
 
 #About Me
 def about_me_section():
     st.header("🚀About Me")
-    st.image(info.profile_picture, width=200)
-    st.write(info.about_me)
+    st.image(infoj.profile_picture, width=200)
+    st.write(infoj.about_me)
     st.write("---")
 about_me_section()
 
@@ -14,13 +14,13 @@ about_me_section()
 def links_section():
     st.sidebar.header("Links")
     st.sidebar.text("Connect with me on LinkedIn")
-    linkedin_link=f'<a href="{info.my_linkedin_url}"><img src="{info.linkedin_image_url}" alt="LinkedIn" width = "75" height = "75"></a>'
+    linkedin_link=f'<a href="{infoj.my_linkedin_url}"><img src="{infoj.linkedin_image_url}" alt="LinkedIn" width = "75" height = "75"></a>'
     st.sidebar.markdown(linkedin_link, unsafe_allow_html=True)
     st.sidebar.text("Checkout my work")
-    github_link=f'<a href="{info.my_github_url}"><img src="{info.github_image_url}" alt="Github" width = "65" height = "65"></a>'
+    github_link=f'<a href="{infoj.my_github_url}"><img src="{infoj.github_image_url}" alt="Github" width = "65" height = "65"></a>'
     st.sidebar.markdown(github_link, unsafe_allow_html=True)
     st.sidebar.text("Or email me!")
-    email_html=f'<a href="mailto:{info.my_email_address}"><img src="{info.email_image_url}" alt="Email" width = "75" height = "75"></a>'
+    email_html=f'<a href="mailto:{infoj.my_email_address}"><img src="{infoj.email_image_url}" alt="Email" width = "75" height = "75"></a>'
     st.sidebar.markdown(email_html, unsafe_allow_html=True)
 links_section()
 
@@ -41,7 +41,7 @@ def education_section(education_data,course_data):
         hide_index=True,
         )
     st.write("---")
-education_section(info.education_data, info.course_data)
+education_section(infoj.education_data, infoj.course_data)
 
 #Professional Experience
 
@@ -53,7 +53,7 @@ def experience_section(experience_data):
         for bullet in job_description:
             expander.write(bullet)
     st.write("---")
-experience_section(info.experience_data)
+experience_section(infoj.experience_data)
 
 #Projects
 
@@ -63,7 +63,7 @@ def project_section(projects_data):
         expander=st.expander(f"{project_name}")
         expander.write(project_description)
     st.write("---")
-project_section(info.projects_data)
+project_section(infoj.projects_data)
 
 #Skills
 
@@ -71,14 +71,14 @@ def skills_section(programming_data, spoken_data):
     st.header("👨‍💻Skills")
     st.subheader("Programming Languages")
     for skill, percentage in programming_data.items():
-        st.write(f"{skill}{info.programming_icons.get(skill,)}")
+        st.write(f"{skill}{infoj.programming_icons.get(skill,)}")
         st.progress(percentage)
     st.subheader("Spoken Languages")
     for spoken, proficiency in spoken_data.items():
-        st.write(f"{spoken}{info.spoken_icons.get(spoken,)}: {proficiency}")
+        st.write(f"{spoken}{infoj.spoken_icons.get(spoken,)}: {proficiency}")
 
     st.write("---")
-skills_section(info.programming_data, info.spoken_data)
+skills_section(infoj.programming_data, infoj.spoken_data)
 
 #Activities
 
@@ -100,5 +100,5 @@ def activities_section(leadership_data,activity_data):
                 expander.write(bullets)
     st.write("---")
 
-activities_section(info.leadership_data, info.activity_data)
+activities_section(infoj.leadership_data, infoj.activity_data)
 
